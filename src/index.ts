589490b0859a77fefa183b8bb0a9135e1e1e76f3
Essien Emmanuel <essienemma300@gmail.com>
2024-07-04 00:03:30 +0100

    added test event and client error handling

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,17 @@ const PORT = 3002;
 const server = createServer(app);
 const io = new Server(server);
 
-io.on("connection", () => {
-  console.log("- Client Connected.");
+io.on("connection", (socket) => {
+  socket.on("connect_error", () => {
+    console.log("- Connection Error.❌");
+  });
+  socket.on("disconnect", () => {
+    console.log("- Client Disconnected");
+  });
+
+  console.log("- Client Connected.✅");
+
+  io.emit("crypto", "Hello cryto guys");
 });
 
 server.listen(PORT, () => {
