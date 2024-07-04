4c253b42e56a3a404ddb8569d2fde79046643e0f
Essien Emmanuel <essienemma300@gmail.com>
2024-07-04 01:28:24 +0100

    fetched crypto list using axios and messari.io api

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
+import { config } from "dotenv";
 import { createServer } from "http";
+import axios from "axios";
 import { Server } from "socket.io";
 import { app } from "./app";
 
-const PORT = 3002;
+config();
+
+const PORT = process.env.PORT;
 const server = createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
   socket.on("connect_error", () => {
-    console.log("- Connection Error.❌");
+    console.log("- Connection Error!❌");
   });
   socket.on("disconnect", () => {
-    console.log("- Client Disconnected");
+    console.log("- Client Disconnected!!!");
   });
 
-  console.log("- Client Connected.✅");
+  console.log("- Client Connected!✅");
 
   io.emit("crypto", "Hello cryto guys");
 });
@@ -22,3 +26,22 @@ io.on("connection", (socket) => {
 server.listen(PORT, () => {
   console.log(`- App Environment:: ${PORT}`);
 });
+
+const fetchCryptoList = async () => {
+  const options = {
+    method: "GET",
+    url: process.env.CRYPTO_LIST_URL,
+    headers: {
+      accept: "application/json",
+    },
+  };
+  const response = await axios.request(options);
+  if (response.status !== 200)
+    console.log(`Error fetching crypto list with code:: ${response.status}`);
+
+  const data = response.data;
+  // console.log(JSON.stringify(response.data, null, 2));
+  console.log(data);
+};
+
+fetchCryptoList();
